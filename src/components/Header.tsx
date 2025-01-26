b0cd0ce2a7a9697f5f16fe02f7f20fa42fe85a58
vinodknaidu <knaidu.vinod@gmail.com>
2025-01-26 09:46:52 +0530

    comment MenuIcon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+// import IconButton from '@mui/material/IconButton';
+// import MenuIcon from '@mui/icons-material/Menu';
 import Typography from '@mui/material/Typography';
 
 const Header: React.FC = () => {
   return (
     <AppBar>
       <Toolbar>
-        <IconButton edge='start' size='large' color='inherit'>
+        {/* <IconButton edge='start' size='large' color='inherit'>
           <MenuIcon />
-        </IconButton>
+        </IconButton> */}
         <Typography variant='h5'>Beyond The Mountains</Typography>
       </Toolbar>
     </AppBar>
